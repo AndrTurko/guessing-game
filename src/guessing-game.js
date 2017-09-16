1e339ce34ffd5d7197dc19911d85804f0c0a5972
Andrew <Sir.Serfer@gmail.com>
2017-09-16 14:13:59 +0300

    Solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,29 @@
+
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.middle = 0;
+        this.array = [];
+    }
 
     setRange(min, max) {
-
+        this.max = max;
+        for (var i = 0; i <= this.max; i++) {
+            this.array.push(i);
+        }
     }
 
     guess() {
-
+        this.middle = Math.floor(this.array.length / 2);
+        return this.array[this.middle];
     }
 
     lower() {
-
+        this.array.splice(this.middle + 1, this.array.length);
     }
 
     greater() {
+        this.array.splice(0, this.middle);
 
     }
 }
-
 module.exports = GuessingGame;
